538723b27c966a5776e981b90be375f7e7763be0
Julius Mwangi <julius.mwangi@moringaschool.com>
2022-10-18 12:39:40 +0300

    commit tree 7cf2d37f3025f42ccc113aa83638a6f5e273f37f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
+require "pry"
 def oxford_comma(array)
-
+    case
+    when array.length == 1 then array.join
+    when array.length == 2 then array.join(" and ")
+    when array.length >= 3 then "#{array[0, array.length-1].join(", ")}, and #{array.last}"
+    end
 end
+
+# binding.pry
